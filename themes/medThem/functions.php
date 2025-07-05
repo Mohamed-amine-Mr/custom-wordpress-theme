ebd1710e5184c51a3c5c10cd7ce7bada538f5c7b
Mohamed-amine-Mr <mohamedaminemounir7@gmail.com>
2025-07-05 07:32:32 +0100

    Enqueue Bootstrap, FontAwesome, custom, and Google Fonts styles in functions.php using wp_enqueue_style

--- themes/medThem/functions.php
+++ themes/medThem/functions.php
@@ -5,6 +5,12 @@
 function wpdocs_mounirThem_scripts()
 {
     wp_enqueue_style('bootstrap', get_template_directory_uri() . '/assets/css/bootstrap.min.css');
+    wp_enqueue_style('templatemo', get_template_directory_uri() . '/assets/css/templatemo.css');
+    wp_enqueue_style('custom', get_template_directory_uri() . '/assets/css/custom.css');
+    wp_enqueue_style('fontawesome', get_template_directory_uri() . '/assets/css/fontawesome.min.css');
+    //   outside link of the font we should use 
+    wp_enqueue_style('fonts',  'https://fonts.googleapis.com/css2?family=Roboto:wght@100;200;300;400;500;700;900&display=swap');
+
 
     wp_enqueue_script('script-name', get_template_directory_uri() . '/js/example.js', array(), '1.0.0', true);
 }
